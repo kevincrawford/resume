2f763f385cca8cbea0fc59c7e97af1481e2edd11
Kevin Crawford <kcrawford@wheels.com>
2015-04-24 13:24:30 -0500

    update scroll to top on page change

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('evertheme')
-  .controller('NavbarCtrl', function ($scope, $location) {
+  .controller('NavbarCtrl', function ($scope, $location, $rootScope) {
     $scope.date = new Date();
 
     $scope.getLoc = function() {
     	return $location.$$path;
     };
+
+    $rootScope.$on('$stateChangeSuccess', function() {
+       document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
 });
